20d5f0e6df03ee2b5baf14380c001cd948cb4a64
Arten Kress <artemkress1@gmail.com>
2023-04-05 11:47:16 +0300

    added chat.id and button

---
+++ bot/chatids.py
@@ -0,0 +1,13 @@
+from os import path
+
+#Проверят создан ли файл chatids.txt
+if not path.exists("chatids.txt"):
+    open("chatids.txt", "w").close()
+#Загрузка массива с файла в SET (Разсылка идет по SET, не по файлу (файл выступает в роли истории))
+chatids_file = open("chatids.txt", "r")
+chatids_users = set ()
+for line in chatids_file:
+    chatids_users.add(line.strip())
+chatids_file.close()
+#Создание словоря с iD и выбранным языком
+user_languages = {}
--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
 import logging
+from chatids import chatids_users
 from aiogram import Bot, Dispatcher, executor, types
 import text
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -12,20 +14,36 @@ load_dotenv(find_dotenv()) #подгрузить файл .env
 bot = Bot(getenv('TEST_TOKEN')) #прочитать фай
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await bot.send_chat_action(message.chat.id, 'typing')
-    await message.answer(text=text.language_selection)
-    await message.answer(text=text.button_driver)
-    await message.answer(text=text.eng_button_driver)
+async def language_selection(message: types.Message):
+    #Если chat.id нет в сете (chatids_users) то добавляет
+    if not str(message.from_user.id) in chatids_users:
+        chatids_users.add(message.from_user.id)
+        #Если chat.id нет в файле (chatids.txt) то добавляет
+        try:
+            if str(message.from_user.id) not in open('chatids.txt').read():
+                chatids_file = open("chatids.txt", "a")
+                chatids_file.write(str(message.from_user.id) + "\n")
+                chatids_file.close()
+        except FileNotFoundError as e:
+            print(e) #---Записать в логинг----
+            #Создание chatids.txt
+            open("chatids.txt", "w").close()
+    await bot.send_chat_action(message.from_user.id, 'typing')
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = types.KeyboardButton(text = "🇺🇦Українська")
+    btn2 = types.KeyboardButton(text ="🇬🇧English")
+    kb.add(btn1, btn2,)
+    await bot.send_message(message.from_user.id, text=text.language_selection, parse_mode="HTML")
+    await bot.send_message(message.from_user.id, text=text.button_driver, parse_mode="HTML")
+    await bot.send_message(message.from_user.id, text=text.eng_button_driver, parse_mode="HTML", reply_markup=kb)
     # await message.delete()
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
     # old style:
-    # await bot.send_message(message.chat.id, message.text)
+    # await bot.send_message(message.from_user.id, message.text)
 
     await message.answer(message.text)
 
